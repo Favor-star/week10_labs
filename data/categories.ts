b1e0df20e69217abbbba451b5f2e35bce9fda3c5
Favor-star <favoureliab@gmail.com>
2025-07-04 14:11:25 +0200

    feat: created a categories seed  and stored the data locally

---
+++ data/categories.ts
@@ -0,0 +1,7 @@
+export default [
+  { id: "01JZANBP6WQMWHSZX4QXQSBZ68", name: "Backend Development" },
+  { id: "01JZANBP6WHGEX15030MJ1VMET", name: "Frontend Development" },
+  { id: "01JZANBP6WVDXG8B40HQ4KDPNT", name: "DevOps" },
+  { id: "01JZANBP6W23N2DNBYK3NV2H0E", name: "UI/UX" },
+  { id: "01JZANBP6WSHV4T53Z09PK2SPK", name: "Others" },
+];
